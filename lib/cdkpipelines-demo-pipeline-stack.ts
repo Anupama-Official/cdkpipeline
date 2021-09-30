e108bcd47890a507204faf817a295d926d28965b
Anupama-Official <satanupa@amazon.com>
2021-09-30 05:34:09 +0000

    Add PreProd stage

--- lib/cdkpipelines-demo-pipeline-stack.ts
+++ lib/cdkpipelines-demo-pipeline-stack.ts
@@ -1,5 +1,6 @@
 import { Construct, SecretValue, Stack, StackProps } from '@aws-cdk/core';
 import { CodePipeline, CodePipelineSource, ShellStep } from "@aws-cdk/pipelines";
+import { CdkpipelinesDemoStage } from './cdkpipelines-demo-stage';
 
 /**
  * The stack that defines the application pipeline
@@ -15,8 +16,9 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
        // How it will be built and synthesized
        synth: new ShellStep('Synth', {
          // Where the source can be found
-         input: CodePipelineSource.gitHub('OWNER/REPO', 'main'),
-         
+         input: CodePipelineSource.connection('Anupama-Official/cdkpipeline', 'main', {
+         connectionArn: 'arn:aws:codestar-connections:us-east-1:968532631134:connection/9da762c0-a715-4406-86d7-05f934ace962',
+         }),
          // Install dependencies, build and run cdk synth
          commands: [
            'npm ci',
@@ -28,5 +30,8 @@ export class CdkpipelinesDemoPipelineStack extends Stack {
 
     // This is where we add the application stages
     // ...
+    pipeline.addStage(new CdkpipelinesDemoStage(this, 'PreProd', {
+  env: { account: '968532631134', region: 'us-east-1' }
+}));
   }
 }
